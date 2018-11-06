41a2660abc6a22fe79b5361648fa1533f4df19e1
Cobalt1911 <antonin.aumetre@gmail.com>
2018-11-06 15:49:00 +0100

    Added a Main.c file

---
+++ Project2/Main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <malloc.h>
+
+#define DEBUG 0
+
+/*=======================================================================================
+*	This code was written by: Antonin Aumètre - antonin.aumetre@gmail.com
+*	For: High Performance Scientific course at ULiège, 2018-19
+*	Project 1
+*
+*	Under GNU General Public License 09/2018
+=======================================================================================*/
+
+
+	//======================= PRE-PROCESSING ===========================//
+	//Reads the content of the image file and stores it in an array
+
+	int main(int argc, char **argv){
+
+	//======================= ALGORITHM ===========================//
+	/*
+	Input :
+		- filter size Fs
+		- filter level Fl
+		- width and height extracted from the file
+		- array pic[width][height]
+	Output :
+		- array containing the filtered image
+	*/
+
+	//======================= POST-PROCESSING ===========================//
+	//Takes the filtred image and saves it as a .ppm
+	
+	//======================= END OF PROGRAM ===========================//
+
+	return(0);
+}
