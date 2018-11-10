f96ab6cba71f1e2a8c23226facbe02c4e6ce7bba
cemoureau <cemoureau@gmail.com>
2018-11-10 12:00:48 +0100

    formula working :)

--- Project2/explicit.c
+++ Project2/explicit.c
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[])
   vz = 0;
   D = 0.5;
 
-  size_t nodeX = (int)(L/h) + 1;
+  size_t nodeX = 4;//(int)(L/h) + 1;
   ///// !!!! vérifier pour les indices pairs le centre!
   size_t nodeY = nodeX, nodeZ =nodeX;
   printf("%zu\n", nodeX);
@@ -37,45 +37,56 @@ int main(int argc, char const *argv[])
 
   size_t centerIndex = nodeX*nodeY*floor(nodeZ/2)+ floor(nodeY/2)*nodeX + floor(nodeX/2);
   printf("%zu\n", centerIndex);
-
-  concentration[centerIndex] = initConcentration;
+  
+  concentrationPrev[centerIndex] = initConcentration;
   //for(int i=0; i<nodeX*nodeY*nodeZ ; i++)
     //printf("%f ", concentration[i]);
 
-  //research for boundaries
-  size_t isXbound = 0;
-  for(size_t index = 0; index<nodeX*nodeY*nodeZ; index++)
+  for(size_t blabla = 0; blabla<3; blabla++)
   {
-    int stage = floor(index/(nodeX*nodeY));
-    int inStage0 = index-stage*nodeX*nodeY;
+    printf("interation %zu\n", blabla);
+    //research for boundaries
+    size_t isXbound = 0;
+    for(size_t index = 0; index<nodeX*nodeY*nodeZ; index++)
+    {
+      int stage = floor(index/(nodeX*nodeY));
+      int inStage0 = index-stage*nodeX*nodeY;
 
-    if(isXbound==0)
-      {}
-    else if(isXbound==nodeX-1)
-      {}
-    else if(inStage0<nodeX)
-      {}
-    else if(inStage0>=nodeX*nodeY-nodeX-1)//vérifier les = !!!!!
-      {}
-    else if(index<=nodeX*nodeY-1)
-      {}
-    else if(index>=nodeX*nodeY*nodeZ-nodeX*nodeY)
-      {}
-    else
-      {//I am in the domain
-        int k = floor(index/(nodeX*nodeY));
-        int j = floor((index-k*nodeX*nodeY)/nodeX);
-        int i = index - k * nodeX * nodeY - j * nodeX;
-        concentration[i+j*nodeX+k*nodeX*nodeY] = concentrationPrev[i+j*nodeX+k*nodeX*nodeY] + m * D * (concentrationPrev[i+1+j*nodeX+k*nodeX*nodeY]+concentrationPrev[i+(j+1)*nodeX+k*nodeX*nodeY]+concentrationPrev[i+j*nodeX+(k+1)*nodeX*nodeY]-6*concentrationPrev[i+j*nodeX+k*nodeX*nodeY]+concentrationPrev[i-1+j*nodeX+k*nodeX*nodeY]+concentrationPrev[i+(j-1)*nodeX+k*nodeX*nodeY]+concentrationPrev[i+j*nodeX+(k-1)*nodeX*nodeY])/pow(h,2) - m * vx * (concentrationPrev[i+1+j*nodeX+k*nodeX*nodeY]-concentrationPrev[i-1+j*nodeX+k*nodeX*nodeY])/(2*h) - m * vy * (concentrationPrev[i+(j+1)*nodeX+k*nodeX*nodeY]-concentrationPrev[i+(j-1)*nodeX+k*nodeX*nodeY])/(2*h) - m * vz * (concentrationPrev[i+j*nodeX+(k+1)*nodeX*nodeY]-concentrationPrev[i+j*nodeX+(k-1)*nodeX*nodeY])/(2*h);
-      }
+      if(isXbound==0)
+        {}
+      else if(isXbound==nodeX-1)
+        {}
+      else if(inStage0<nodeX)
+        {}
+      else if(inStage0>=nodeX*nodeY-nodeX-1)//vérifier les = !!!!!
+        {}
+      else if(index<=nodeX*nodeY-1)
+        {}
+      else if(index>=nodeX*nodeY*nodeZ-nodeX*nodeY)
+        {}
+      else
+        {//I am in the domain
+          int k = floor(index/(nodeX*nodeY));
+          int j = floor((index-k*nodeX*nodeY)/nodeX);
+          int i = index - k * nodeX * nodeY - j * nodeX;
+          concentration[i+j*nodeX+k*nodeX*nodeY] = concentrationPrev[i+j*nodeX+k*nodeX*nodeY] + m * D * (concentrationPrev[i+1+j*nodeX+k*nodeX*nodeY]+concentrationPrev[i+(j+1)*nodeX+k*nodeX*nodeY]+concentrationPrev[i+j*nodeX+(k+1)*nodeX*nodeY]-6*concentrationPrev[i+j*nodeX+k*nodeX*nodeY]+concentrationPrev[i-1+j*nodeX+k*nodeX*nodeY]+concentrationPrev[i+(j-1)*nodeX+k*nodeX*nodeY]+concentrationPrev[i+j*nodeX+(k-1)*nodeX*nodeY])/pow(h,2) - m * vx * (concentrationPrev[i+1+j*nodeX+k*nodeX*nodeY]-concentrationPrev[i-1+j*nodeX+k*nodeX*nodeY])/(2*h) - m * vy * (concentrationPrev[i+(j+1)*nodeX+k*nodeX*nodeY]-concentrationPrev[i+(j-1)*nodeX+k*nodeX*nodeY])/(2*h) - m * vz * (concentrationPrev[i+j*nodeX+(k+1)*nodeX*nodeY]-concentrationPrev[i+j*nodeX+(k-1)*nodeX*nodeY])/(2*h);
+        }
 
-    isXbound++;
-    if(isXbound==nodeX)
-        isXbound = 0;
+      isXbound++;
+      if(isXbound==nodeX)
+          isXbound = 0;
+    }
+
+    for(int i=0; i<nodeX*nodeY*nodeZ ; i++)
+    {
+      concentrationPrev[i] = concentration[i];
+      printf("%f ", concentration[i]);
+    }
+
+    printf("\n \n \n");
   }
 
-  for(int i=0; i<nodeX*nodeY*nodeZ ; i++)
-    printf("%f ", concentration[i]);
+
 
 
   return 0;
