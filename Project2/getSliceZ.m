351bdf455325c9110892ca1f0708dd177b8fbc23
cemoureau <cemoureau@gmail.com>
2018-11-18 16:01:00 +0100

    idem mais ça avait pas push

---
+++ Project2/getSliceZ.m
@@ -0,0 +1,16 @@
+% Get values at slice 'z' for the time step 'timestep'
+% /!\ File must be located in directory 'results' and must be named
+% 'c_X.dat' where 'X' is the timestep number.
+%
+function v = getSliceZ(timeStep, z)
+    fid = fopen(['results/c_' num2str(timeStep) '.dat'], 'r');
+    N = fread(fid,1,'int32');
+    data = fread(fid,N*N*N,'double');
+    v = zeros(N,N);
+    for a = 1:1:N
+        for b = 1:1:N
+            v(a,b) = data(b + N*(a-1)+ N*N*z);
+        end
+    end
+    fclose(fid);
+end
