37f46799603d7411e839b0e257ff2a4919421bb0
cemoureau <cemoureau@gmail.com>
2018-11-19 21:19:56 +0100

    boundary and boundary-1 print index to check

--- Project2/explicit.c
+++ Project2/explicit.c
@@ -152,7 +152,7 @@ int main(int argc, char *argv[])
 				onBoundary = ((isXbound == nodeX-2) || (isXbound == 1) || (inStage0 >= nodeX && inStage0 <= 2*nodeX-1) || (inStage0 >= nodeY*nodeY-2*nodeX));
 				//printf("onZ %d, onbound %d from process %d\n", onZBoundary, onBoundary, rank);
 				if ((onBoundary || onZBoundary)  && (concentration[i+j*nodeX+k*nodeX*nodeY] != 0)) //valueOnBoundary=true;
-				{printf("onbound comparison from process %d\n", rank); valueOnBoundary=true; printf("onbound comparison works from process %d\n", rank);}
+				{valueOnBoundary=true; printf("onbound comparison works at index %zu for process %d\n", index, rank);}
 
 			}
 
