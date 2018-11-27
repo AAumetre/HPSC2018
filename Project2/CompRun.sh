962dfa03fc230a73e1e9f9df1280edb8438282dd
cemoureau <cemoureau@gmail.com>
2018-11-27 13:56:36 +0100

    je sais plus ce que j ai fait

--- Project2/CompRun.sh
+++ Project2/CompRun.sh
@@ -2,8 +2,8 @@ clear
 #mpicc -g -o Main explicitCSR.c -lm -std=c99 -fopenmp
 #mpiexec -np 4 time ./Main param_small.dat
 
-mpicc -g -o Main explicit.c -lm -std=c99 -fopenmp
-mpiexec -np 16 time ./Main param_medium.dat
+mpicc -g -o Main octopus.c -lm -std=c99 -fopenmp
+mpiexec -np 11 time ./Main param_medium.dat
 
 #mpicc -g -o Main explicitCSR.c -lm -std=c99 -fopenmp
 #mpiexec -np 1 time ./Main param_medium.dat
