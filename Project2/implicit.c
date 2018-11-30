526b4acc14066b7e658be54c0e8d54c791a4b89a
cemoureau <cemoureau@gmail.com>
2018-11-30 17:33:54 +0100

    start implicit

--- Project2/implicit.c
+++ Project2/implicit.c
@@ -82,6 +82,49 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	double *A= calloc(nodeX*nodeY*nodeZ*nodeX*nodeY*nodeZ, sizeof(double));
+	if (A == NULL)
+	{
+		puts("Mem ERR0R !");
+		exit(1);
+	}
+
+	for(size_t i=0; i<nodeX, i++)
+	{
+		for (size_t j = 0; j < nodeY; j++)
+		{
+			for (size_t k = 0; k < nodeZ; k++)
+			{
+				size_t line = i + nodeX*j + nodeX*nodeY*k; //line i j k
+				// i j k
+				size_t column = i + nodeX*j + nodeX*nodeY*k;
+				A[line*nodeX*nodeY*nodeZ + column]+= 1/parameters.m + 6*parameters.D/(parameters.h*parameters.h);
+				// i-1 j k
+				column = i-1 + nodeX*j + nodeX*nodeY*k;
+				A[line*nodeX*nodeY*nodeZ + column]-= parameters.vx/(2*h) + parameters.D/(parameters.h*parameters.h);
+				// i+1 j k
+				column = i+1 + nodeX*j + nodeX*nodeY*k;
+				A[line*nodeX*nodeY*nodeZ + column]+= parameters.vx/(2*h) - parameters.D/(parameters.h*parameters.h);
+				// i j-1 k
+				column = i + nodeX*(j-1) + nodeX*nodeY*k;
+				A[line*nodeX*nodeY*nodeZ + column]-= parameters.vy/(2*h) + parameters.D/(parameters.h*parameters.h);
+				// i j+1 k
+				column = i + nodeX*(j+1) + nodeX*nodeY*k;
+				A[line*nodeX*nodeY*nodeZ + column]+= parameters.vy/(2*h) - parameters.D/(parameters.h*parameters.h);
+				// i j k-1
+				column = i + nodeX*j + nodeX*nodeY*(k-1);
+				A[line*nodeX*nodeY*nodeZ + column]-= parameters.vz/(2*h) + parameters.D/(parameters.h*parameters.h);
+				// i j k+1
+				column = i + nodeX*j + nodeX*nodeY*(k+1);
+				A[line*nodeX*nodeY*nodeZ + column]+= parameters.vz/(2*h) - parameters.D/(parameters.h*parameters.h);
+
+			}
+		}
+	}
+
+	bsr_matrix Absr;
+	convert_natural_to_bsr(A, &Absr, sqrt(sizeof(A)/sizeof(A[0])), 3); // Convert it to BSR
+
 	// If there are some idling nodes, exclude them from the new communicator
 	MPI_Group world_group;
 	MPI_Comm_group(MPI_COMM_WORLD, &world_group);
@@ -116,6 +159,9 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
+	double *residu = calloc(nodeX*nodeY*thicknessMPI, sizeof(double));
+	double *p = calloc(nodeX*nodeY*thicknessMPI, sizeof(double));
+
 	// Give initial concentration value
 	int middleSliceIndex = floor(nodeZ/2);
 	int rankMiddle = floor(middleSliceIndex/(thicknessMPI-nbAdditionalSlices));
@@ -124,15 +170,21 @@ int main(int argc, char *argv[])
 		c_[initValueIndex] = initConcentration;
 		printf("Initial value set on node #%d, at index %d\n", rank, initValueIndex);
 	}
+
 	MPI_Barrier(MPI_COMM_WORLD);
+	size_t currentSliceLower = rank*thicknessMPI-nbAdditionalSlices;
+	size_t currentSliceUpper = currentSliceLower + thicknessMPI-1;
+	//to help finding the good column of A
 
 	/*================================================================================================
 	#	Main loop
 	================================================================================================*/
 	if (rank == 0) printf("Iteration: ");
-	while (iteration <= stopTime && !stopFlag){
+	while (iteration <= stopTime && !stopFlag)
+	{
 		if (rank == 0) printf("%ld ", iteration);
-		// Search for boundaries
+
+		/*// Search for boundaries
 		size_t isXbound = 0;
 		size_t index = 0;
 		if (rank == 0) index = nodeX*nodeY;
@@ -182,12 +234,14 @@ int main(int argc, char *argv[])
 			if(valueOnBoundary)stopFlags[i] = 1;
 		}
 		MPI_Allgather(stopFlags, 1, MPI_INT, stopFlagsFromOthers, 1, MPI_INT, SUB_COMM);
-		for (int i = 0; i < world_size; ++i){
-			if(stopFlagsFromOthers[i] == 1){
+		for (int i = 0; i < world_size; ++i)
+		{
+			if(stopFlagsFromOthers[i] == 1)
+			{
 			stopFlag = true;
 			break;
 			}
-		}
+		}*/
 
 		// ============================== Send and receive neighboring values
 		int *commList = getCommListSlices(world_size);
