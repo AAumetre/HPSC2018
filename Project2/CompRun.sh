38944f27d45c852a3d451e674b086db437ebad2f
cemoureau <cemoureau@gmail.com>
2018-12-11 15:50:55 +0100

    change comprun

--- Project2/CompRun.sh
+++ Project2/CompRun.sh
@@ -3,13 +3,12 @@ clear
 #gcc CSR_BSR_example.c COO_CSR_BSR.c algorithms.c -o Main -lm -std=c99
 #./Main
 
-#module load openmpi/1.8.4/gcc-4.9.2
+module load openmpi/1.8.4/gcc-4.9.2
 
-set OMP_NUM_THREADS = $1
-export OMP_NUM_THREADS
+OMP_NUM_THREADS=3
 
 mpicc -g -o Octopus octopus.c explicit.c implicit.c COO_CSR_BSR.c fileIO.c algorithms.c -lm -std=c99 -fopenmp
-mpiexec -np 1 ./Octopus param_medium.dat 0
+mpiexec -np 2 time ./Octopus param_medium.dat 0
 
 #mpicc -g -o Octopus octopus.c explicit.c implicit.c COO_CSR_BSR.c fileIO.c algorithms.c -lm -std=c99 -fopenmp
 #mpiexec -np 4 ./Octopus param_medium.dat 1
