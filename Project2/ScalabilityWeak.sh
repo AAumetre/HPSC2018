2e73a890fc0bfbbd980958c4c590e872ffe671b3
cemoureau <cemoureau@gmail.com>
2018-12-15 12:22:06 +0100

    sh weak for implicit

--- Project2/ScalabilityWeak.sh
+++ Project2/ScalabilityWeak.sh
@@ -21,28 +21,28 @@ module load openmpi/1.8.4/gcc-4.9.2
 
 #mpicc -g -o Octopus octopus.c explicit.c implicit.c COO_CSR_BSR.c fileIO.c algorithms.c -O0 -lm -std=c99 -fopenmp
 
-echo "Explicit";
+echo "Implicit";
 
 echo "Tiny";
 t0=$(date +%s%3N)
-mpiexec ./Octopus param_tiny.dat 0;
+mpiexec ./Octopus param_tiny.dat 1;
 t1=$(date +%s%3N)
 echo "Elapsed wall clock time = " $(( $t1-$t0 )) " milliseconds."
 
 echo "Small";
 t0=$(date +%s%3N)
-mpiexec ./Octopus param_small.dat 0;
+mpiexec ./Octopus param_small.dat 1;
 t1=$(date +%s%3N)
 echo "Elapsed wall clock time = " $(( $t1-$t0 )) " milliseconds."
 
 echo "Medium";
 t0=$(date +%s%3N)
-mpiexec ./Octopus param_medium.dat 0;
+mpiexec ./Octopus param_medium.dat 1;
 t1=$(date +%s%3N)
 echo "Elapsed wall clock time = " $(( $t1-$t0 )) " milliseconds."
 
 echo "Big";
 t0=$(date +%s%3N)
-mpiexec ./Octopus param_big.dat 0;
+mpiexec ./Octopus param_big.dat 1;
 t1=$(date +%s%3N)
 echo "Elapsed wall clock time = " $(( $t1-$t0 )) " milliseconds."
